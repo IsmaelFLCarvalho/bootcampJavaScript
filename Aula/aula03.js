7e36a66b6e82ca2c9aa6e708b4ff3b835f0ef65b
Ismael Carvalho <ismaelcarvalhopt@gmail.com>
2024-09-27 21:47:08 +0100

    aulas 3 e 4( objeto e classes )

---
+++ Aula/aula03.js
@@ -0,0 +1,18 @@
+//OBJETO
+
+const ismael = {
+    nome : 'Ismael Carvalho',
+    idade : 30,
+    altura : 1.77,
+    peso : 96,
+    cor : 'parda'
+};
+
+console.log(ismael.idade); //retorna apenas a idade
+console.log(ismael.nome); //retorna apenas o nome
+console.log(ismael.altura); //retorna apenas a altura
+console.log(ismael.peso); //retorna apenas o peso
+console.log(ismael.cor); //retorna apenas a cor
+console.log(ismael); //retorna todos os dados.
+
+
---
+++ Aula/aula04.js
@@ -0,0 +1,21 @@
+//classes
+
+class person { 
+    name; 
+    age;
+
+        description(){
+            console.log(`My name is ${this.name}, and my age is ${this.age} years old.`);
+        }
+}
+
+const ismael = new person();
+ismael.name = 'Ismael Carvalho';
+ismael.age = 30;
+
+const jhersica = new person();
+jhersica.name = 'Jhersica Fernandes';
+jhersica.age = 33;
+
+ismael.description();
+jhersica.description();
